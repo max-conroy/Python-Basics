89947a429299b5077c14a2d4c5939c0ee40c7544
Max Conroy <max.conroy@outlook.com>
2021-11-24 14:46:26 -0500

    Init

--- main.py
+++ main.py
@@ -2,14 +2,16 @@
 
 def convert_celsius(fahr):
     # Program to convert Fahrenheit to Celsius
-    cel = (5/9)*(fahr - 32)
+    cel = (5 / 9) * (fahr - 32)
     print("It is {:.1f} degrees celsius.".format(cel))
+
 
 def calc_tax(cost):
     # Program to find NY sales tax on a purchase
     tax = cost * 8.875
     tax = tax * .01
     print("Your NY sales tax is {:.2f}: ".format(tax))
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
